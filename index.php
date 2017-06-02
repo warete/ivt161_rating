33ba13a2de88428a32c451f755920b53dd3683c2
warete <mr-warete@mail.ru>
2017-06-03 00:15:09 +0300

    до редизайна

--- index.php
+++ index.php
@@ -80,9 +80,9 @@ function get_results($id, $out, $phio)
         echo "<td>{$curr_str}</td>";
         //$TextMessage .= "<tr><td>{$subjects[$i]}</td><td>{$curr_str}</td></tr>";
         $total .= $subjects[$i]." - ".trim($curr_str)."\n\r";  
-        $points += 
+        $points += $curr_str;
     }
-    echo "<tr><td>{$curr_str}</td>";
+    echo "<td>{$points}</td>";
     echo "</tr>";
 }
     
@@ -110,7 +110,7 @@ function get_subjects($out){
         //$TextMessage .= "<tr><td>{$subjects[$i]}</td><td>{$curr_str}</td></tr>";
         $total .= $subjects[$i]." - ".trim($curr_str)."\n\r";    
     }
-    echo "<td onclick='sort(this)'>Всего баллов</td>";
+    echo "<td onclick='sort(this)'><b>Всего баллов</b></td>";
     echo "</tr>";
 }
     if(isset($_GET['sem'])){
@@ -383,58 +383,58 @@ echo "</table>";
 }
         
         (function($){
-	$.fn.liveFilter = function (aType) {
-		
-		// Определяем, что будет фильтроваться.
-		var filterTarget = $(this);
-		var child;
-		if ($(this).is('ul')) {
-			child = 'li';
-		} else if ($(this).is('ol')) {
-			child = 'li';
-		} else if ($(this).is('table')) {
-			child = 'tbody tr';
-		}
-		
-		// Определяем переменные
-		var hide;
-		var show;
-		var filter;
-		
-		// Событие для элемента ввода
-		$('input.filter').keyup(function() {
-			
-			// Получаем значение фильтра
-			filter = $(this).val();
-			
-			// Получаем то, что нужно спрятать, и то, что нужно показать
-			hide = $(filterTarget).find(child + ':not(:Contains("' + filter + '"))');
-			show = $(filterTarget).find(child + ':Contains("' + filter + '")')
-			
-			// Анимируем пункты, которые нужно спрятать и показать
-			if ( aType == 'basic' ) {
-				hide.hide();
-				show.show();
-			} else if ( aType == 'slide' ) {
-				hide.slideUp(500);
-				show.slideDown(500);
-			} else if ( aType == 'fade' ) {
-				hide.fadeOut(400);
-				show.fadeIn(400);                                
-			}            
-			
-		});		
-		
-		// Пользовательское выражение для нечувствительной к регистру текста функции contains()
-		jQuery.expr[':'].Contains = function(a,i,m){
-		    return jQuery(a).text().toLowerCase().indexOf(m[3].toLowerCase())>=0;
-		}; 
+    $.fn.liveFilter = function (aType) {
+        
+        // Определяем, что будет фильтроваться.
+        var filterTarget = $(this);
+        var child;
+        if ($(this).is('ul')) {
+            child = 'li';
+        } else if ($(this).is('ol')) {
+            child = 'li';
+        } else if ($(this).is('table')) {
+            child = 'tbody tr';
+        }
+        
+        // Определяем переменные
+        var hide;
+        var show;
+        var filter;
+        
+        // Событие для элемента ввода
+        $('input.filter').keyup(function() {
+            
+            // Получаем значение фильтра
+            filter = $(this).val();
+            
+            // Получаем то, что нужно спрятать, и то, что нужно показать
+            hide = $(filterTarget).find(child + ':not(:Contains("' + filter + '"))');
+            show = $(filterTarget).find(child + ':Contains("' + filter + '")')
+            
+            // Анимируем пункты, которые нужно спрятать и показать
+            if ( aType == 'basic' ) {
+                hide.hide();
+                show.show();
+            } else if ( aType == 'slide' ) {
+                hide.slideUp(500);
+                show.slideDown(500);
+            } else if ( aType == 'fade' ) {
+                hide.fadeOut(400);
+                show.fadeIn(400);                                
+            }            
+            
+        });     
+        
+        // Пользовательское выражение для нечувствительной к регистру текста функции contains()
+        jQuery.expr[':'].Contains = function(a,i,m){
+            return jQuery(a).text().toLowerCase().indexOf(m[3].toLowerCase())>=0;
+        }; 
 
-	}
+    }
 
 })(jQuery);
         $(document).ready(function() {
-		$('table.live_filter').liveFilter('fade');
+        $('table.live_filter').liveFilter('fade');
             
         function y2k(number) { return (number < 1000) ? number + 1900 : number; }
         function getWeek(year,month,day) {
@@ -504,7 +504,7 @@ echo "</table>";
             }
         }
         
-	});
+    });
     </script>
     <!-- Yandex.Metrika counter -->
 <script type="text/javascript">
